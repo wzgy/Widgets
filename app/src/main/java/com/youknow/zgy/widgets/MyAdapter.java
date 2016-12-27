fd3429358ec2c41b41a5687e1a86bc9a95e8d71f
wzgy <wzgy123456789@hotmail.com>
2016-12-27 13:05:38 +0800

    创建RecyclerView图片展示控件

---
+++ app/src/main/java/com/youknow/zgy/widgets/MyAdapter.java
@@ -0,0 +1,41 @@
+package com.youknow.zgy.widgets;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private String[] mDataset;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView mTextView;
+        public ViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
+    }
+
+    public MyAdapter(String[] mDataset){
+        mDataset = mDataset;
+    }
+
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,int viewType){
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.my_text_view,parent,false);
+        TextView tv=(TextView)v.findViewById(R.id.my_text);
+        return new ViewHolder(tv);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mTextView.setText(mDataset[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
