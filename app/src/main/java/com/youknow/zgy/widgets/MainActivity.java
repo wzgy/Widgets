b5f812257895bd27543393409dd1738fe3dcd781
wzgy <wzgy123456789@hotmail.com>
2016-12-28 00:52:04 +0800

    调整类私有成员变量为局部变量，以避免警告

--- app/src/main/java/com/youknow/zgy/widgets/MainActivity.java
+++ app/src/main/java/com/youknow/zgy/widgets/MainActivity.java
@@ -6,23 +6,20 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
         mRecyclerView.setHasFixedSize(true);
 
-        mLayoutManager = new LinearLayoutManager(this);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
         String[] myDataset={"abc","def"};
-        mAdapter = new MyAdapter(myDataset);
+        RecyclerView.Adapter mAdapter = new MyAdapter(myDataset);
         mRecyclerView.setAdapter(mAdapter);
     }
 }
